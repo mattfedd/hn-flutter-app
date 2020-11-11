44e51b9e7149926a41ba5d2990fb8028cb1b0820
mattfedd <matthew.feddersen@steelseries.com>
2020-11-11 10:55:05 -0600

    Add more tests for Item JSON decoding

--- hacker_news/test/item_test.dart
+++ hacker_news/test/item_test.dart
@@ -12,7 +12,7 @@ void main() {
 
   test('Story with example JSON works', () {
     // same example from the api docs
-    final storyExampleJSON = '''
+    final storyExampleJSON = r'''
 {
   "by" : "dhouston",
   "descendants" : 71,
@@ -80,7 +80,7 @@ void main() {
   });
 
   test('Comment with example JSON works', () {
-    final commentExampleJSON = '''
+    final commentExampleJSON = r'''
 {
   "by" : "norvig",
   "id" : 2921983,
@@ -103,5 +103,143 @@ void main() {
     expect(comment.title, null);
     expect(comment.text,
         "Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?");
+  });
+
+  test('Ask with example JSON works', () {
+    final askExampleJSON = r'''
+{
+  "by" : "tel",
+  "descendants" : 16,
+  "id" : 121003,
+  "kids" : [ 121016, 121109, 121168 ],
+  "score" : 25,
+  "text" : "<i>or</i> HN: the Next Iteration<p>I get the impression that with Arc being released a lot of people who never had time for HN before are suddenly dropping in more often. (PG: what are the numbers on this? I'm envisioning a spike.)<p>Not to say that isn't great, but I'm wary of Diggification. Between links comparing programming to sex and a flurry of gratuitous, ostentatious  adjectives in the headlines it's a bit concerning.<p>80% of the stuff that makes the front page is still pretty awesome, but what's in place to keep the signal/noise ratio high? Does the HN model still work as the community scales? What's in store for (++ HN)?",
+  "time" : 1203647620,
+  "title" : "Ask HN: The Arc Effect",
+  "type" : "story"
+}
+  ''';
+
+    final ask = Item.fromJson(jsonDecode(askExampleJSON));
+
+    expect(ask.id, 121003);
+    expect(ask.by, "tel");
+    expect(ask.descendants, 16);
+    expect(ask.kids, [121016, 121109, 121168]);
+    expect(ask.score, 25);
+    expect(ask.title, "Ask HN: The Arc Effect");
+    expect(ask.type, ItemType.Story);
+    expect(ask.time, DateTime.utc(2008, 2, 22, 2, 33, 40));
+    expect(ask.text,
+        "<i>or</i> HN: the Next Iteration<p>I get the impression that with Arc being released a lot of people who never had time for HN before are suddenly dropping in more often. (PG: what are the numbers on this? I'm envisioning a spike.)<p>Not to say that isn't great, but I'm wary of Diggification. Between links comparing programming to sex and a flurry of gratuitous, ostentatious  adjectives in the headlines it's a bit concerning.<p>80% of the stuff that makes the front page is still pretty awesome, but what's in place to keep the signal/noise ratio high? Does the HN model still work as the community scales? What's in store for (++ HN)?");
+  });
+
+  test('Job with example JSON works', () {
+    final jobExampleJSON = r'''
+{
+  "by" : "justin",
+  "id" : 192327,
+  "score" : 6,
+  "text" : "Justin.tv is the biggest live video site online. We serve hundreds of thousands of video streams a day, and have supported up to 50k live concurrent viewers. Our site is growing every week, and we just added a 10 gbps line to our colo. Our unique visitors are up 900% since January.<p>There are a lot of pieces that fit together to make Justin.tv work: our video cluster, IRC server, our web app, and our monitoring and search services, to name a few. A lot of our website is dependent on Flash, and we're looking for talented Flash Engineers who know AS2 and AS3 very well who want to be leaders in the development of our Flash.<p>Responsibilities<p><pre><code>    * Contribute to product design and implementation discussions\n    * Implement projects from the idea phase to production\n    * Test and iterate code before and after production release \n</code></pre>\nQualifications<p><pre><code>    * You should know AS2, AS3, and maybe a little be of Flex.\n    * Experience building web applications.\n    * A strong desire to work on website with passionate users and ideas for how to improve it.\n    * Experience hacking video streams, python, Twisted or rails all a plus.\n</code></pre>\nWhile we're growing rapidly, Justin.tv is still a small, technology focused company, built by hackers for hackers. Seven of our ten person team are engineers or designers. We believe in rapid development, and push out new code releases every week. We're based in a beautiful office in the SOMA district of SF, one block from the caltrain station. If you want a fun job hacking on code that will touch a lot of people, JTV is for you.<p>Note: You must be physically present in SF to work for JTV. Completing the technical problem at <a href=\"http://www.justin.tv/problems/bml\" rel=\"nofollow\">http://www.justin.tv/problems/bml</a> will go a long way with us. Cheers!",
+  "time" : 1210981217,
+  "title" : "Justin.tv is looking for a Lead Flash Engineer!",
+  "type" : "job",
+  "url" : ""
+}
+  ''';
+
+    final job = Item.fromJson(jsonDecode(jobExampleJSON));
+
+    expect(job.by, "justin");
+    expect(job.id, 192327);
+    expect(job.score, 6);
+    expect(job.url, "");
+    expect(job.type, ItemType.Job);
+    expect(job.title, "Justin.tv is looking for a Lead Flash Engineer!");
+    expect(job.time, DateTime.utc(2008, 5, 16, 23, 40, 17));
+    expect(job.text,
+        '''Justin.tv is the biggest live video site online. We serve hundreds of thousands of video streams a day, and have supported up to 50k live concurrent viewers. Our site is growing every week, and we just added a 10 gbps line to our colo. Our unique visitors are up 900% since January.<p>There are a lot of pieces that fit together to make Justin.tv work: our video cluster, IRC server, our web app, and our monitoring and search services, to name a few. A lot of our website is dependent on Flash, and we're looking for talented Flash Engineers who know AS2 and AS3 very well who want to be leaders in the development of our Flash.<p>Responsibilities<p><pre><code>    * Contribute to product design and implementation discussions\n    * Implement projects from the idea phase to production\n    * Test and iterate code before and after production release \n</code></pre>\nQualifications<p><pre><code>    * You should know AS2, AS3, and maybe a little be of Flex.\n    * Experience building web applications.\n    * A strong desire to work on website with passionate users and ideas for how to improve it.\n    * Experience hacking video streams, python, Twisted or rails all a plus.\n</code></pre>\nWhile we're growing rapidly, Justin.tv is still a small, technology focused company, built by hackers for hackers. Seven of our ten person team are engineers or designers. We believe in rapid development, and push out new code releases every week. We're based in a beautiful office in the SOMA district of SF, one block from the caltrain station. If you want a fun job hacking on code that will touch a lot of people, JTV is for you.<p>Note: You must be physically present in SF to work for JTV. Completing the technical problem at <a href=\"http://www.justin.tv/problems/bml\" rel=\"nofollow\">http://www.justin.tv/problems/bml</a> will go a long way with us. Cheers!''');
+  });
+
+  test('Poll with example JSON works', () {
+    final pollExampleJSON = r'''
+{
+  "by" : "pg",
+  "descendants" : 54,
+  "id" : 126809,
+  "kids" : [ 126822, 126823, 126993, 126824, 126934, 127411, 126888, 127681, 126818, 126816, 126854, 127095, 126861, 127313, 127299, 126859, 126852, 126882, 126832, 127072, 127217, 126889, 127535, 126917, 126875 ],
+  "parts" : [ 126810, 126811, 126812 ],
+  "score" : 46,
+  "text" : "",
+  "time" : 1204403652,
+  "title" : "Poll: What would happen if News.YC had explicit support for polls?",
+  "type" : "poll"
+}
+  ''';
+
+    final poll = Item.fromJson(jsonDecode(pollExampleJSON));
+
+    expect(poll.by, "pg");
+    expect(poll.descendants, 54);
+    expect(poll.id, 126809);
+    expect(poll.score, 46);
+    expect(poll.text, "");
+    expect(poll.title,
+        "Poll: What would happen if News.YC had explicit support for polls?");
+    expect(poll.type, ItemType.Poll);
+    expect(poll.time, DateTime.utc(2008, 3, 1, 20, 34, 12));
+    expect(poll.parts, [126810, 126811, 126812]);
+    expect(poll.kids, [
+      126822,
+      126823,
+      126993,
+      126824,
+      126934,
+      127411,
+      126888,
+      127681,
+      126818,
+      126816,
+      126854,
+      127095,
+      126861,
+      127313,
+      127299,
+      126859,
+      126852,
+      126882,
+      126832,
+      127072,
+      127217,
+      126889,
+      127535,
+      126917,
+      126875
+    ]);
+  });
+
+  test('Pollopt with example JSON works', () {
+    final polloptExampleJSON = r'''
+{
+  "by" : "pg",
+  "id" : 160705,
+  "poll" : 160704,
+  "score" : 335,
+  "text" : "Yes, ban them; I'm tired of seeing Valleywag stories on News.YC.",
+  "time" : 1207886576,
+  "type" : "pollopt"
+}
+  ''';
+
+    final pollopt = Item.fromJson(jsonDecode(polloptExampleJSON));
+
+    expect(pollopt.by, "pg");
+    expect(pollopt.id, 160705);
+    expect(pollopt.poll, 160704);
+    expect(pollopt.score, 335);
+    expect(pollopt.text,
+        "Yes, ban them; I'm tired of seeing Valleywag stories on News.YC.");
+    expect(pollopt.type, ItemType.PollOpt);
+    expect(pollopt.time, DateTime.utc(2008, 4, 11, 4, 2, 56));
   });
 }
